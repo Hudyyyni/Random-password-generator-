19f809d2777f3ae48ab23528ef313137dc5226ef
Hudyyyni <60985440+Hudyyyni@users.noreply.github.com>
2021-09-17 15:43:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,26 @@
 
 int main()
 {
+    int leng{};
+    int amount{};
 
-    RandomPassGenerator pass;
-   
-    pass.generate(10);
-    std::cout << pass;
+    std::cout << "How many password would you like to generate? " << '\n';
+    std::cout << "Amount:";
+    std::cin >> amount;
+    std::cout << "Length:";
+    std::cin >> leng;
+    std::cout << '\n';
+ 
+        std::vector<RandomPassGenerator> arr{};
+      
+        for (int i{ 0 }; i < amount; ++i)
+            arr.push_back(RandomPassGenerator(leng));
 
+        for (const auto& i : arr)
+            std::cout << i << '\n';
+    
+ 
 
 
     return 0;
 }
-
